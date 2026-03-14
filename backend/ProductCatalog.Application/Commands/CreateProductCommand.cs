29adfd92b79e2408d5cddf5fe04d64a71b4b7e64
jtrajano <jlt212000@yahoo.com>
2026-03-15 06:58:05 +0800

    setup application layer

---
+++ backend/ProductCatalog.Application/Commands/CreateProductCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductCatalog.Application.Commands;
+
+public record CreateProductCommand
+(
+    string Name,
+    decimal Price,
+    int Quantity
+);
+
+
---
+++ backend/ProductCatalog.Application/Queries/QueryProduct.cs
@@ -0,0 +1,6 @@
+﻿namespace ProductCatalog.Application.Queries;
+
+public class QueryProduct
+{
+
+}
